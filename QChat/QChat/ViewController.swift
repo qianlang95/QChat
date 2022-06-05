685479614803e42aff528ae5f0b7a90e39a95fef
qianlang95 <qianlang95@gmail.com>
2022-06-05 10:22:02 -0700

    restart

---
+++ QChat/QChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QChat
+//
+//  Created by 郎倩 on 6/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
