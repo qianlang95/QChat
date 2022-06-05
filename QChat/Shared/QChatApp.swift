d107012c149b5ed02990573a823c6a17d3cd542e
qianlang95 <qianlang95@gmail.com>
2022-06-05 10:14:39 -0700

    update

---
+++ QChat/Shared/QChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  QChatApp.swift
+//  Shared
+//
+//  Created by 郎倩 on 6/5/22.
+//
+
+import SwiftUI
+
+@main
+struct QChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
