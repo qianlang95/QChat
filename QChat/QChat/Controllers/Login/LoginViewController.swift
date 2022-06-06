21b4107e54b0695fcca042408e9c9dc1261a3724
qianlang95 <qianlang95@gmail.com>
2022-06-05 23:12:40 -0700

    update

--- QChat/QChat/Controllers/Login/LoginViewController.swift
+++ QChat/QChat/Controllers/Login/LoginViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +25,16 @@ class LoginViewController: UIViewController {
                                                             style: .done,
                                                             target: self,
                                                             action: #selector(didTapRegister))
-        
+        view.addSubview(imageView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let size = view.width/3
+        imageView.frame = CGRect(x: (view.width - size)/2,
+                                 y: 20,
+                                 width: size,
+                                 height: size)
     }
     
     @objc private func didTapRegister(){
